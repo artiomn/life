20f89ffc0d23ab904cac558b5db3a8b05cab8c3b
Artiom N <artiom14@yandex.ru>
2018-09-01 16:12:41 +0300

    Engine class implementation was started

--- engine/include/life_engine.h
+++ engine/include/life_engine.h
@@ -7,22 +7,28 @@
 
 
 template<typename ElementType>
-class LifeEngine
+class LifeEngineBase
 {
 public:
     typedef ElementType e_type;
-    typedef std::function<e_type(size_t, size_t, const e_type&)> dist_funct;
+    typedef std::function<e_type(size_t, size_t, const LifeEngineBase&)> dist_funct;
 
 public:
-    LifeEngine(std::size_t field_width, std::size_t field_height, dist_funct f);
+    LifeEngineBase(std::size_t field_width, std::size_t field_height, dist_funct f);
     void step(long step_size = 1);
-    const e_type& get_cell(size_t x, size_t y) const;
+
+    const e_type& get_cell(size_t x, size_t y) const { return field_[x * width_ + y]; }
+    const size_t get_field_width() const { return width_; }
+    const size_t get_field_height() const { return height_; }
 
 protected:
     void make_one_step(bool forward);
 
 private:
-    const size_t width;
-    const size_t height;
-    std::vector<bool> field_;
+    const size_t width_;
+    const size_t height_;
+    std::vector<e_type> field_;
 };
+
+
+typedef LifeEngineBase<bool> LifeEngine;
--- engine/src/life_engine.cpp
+++ engine/src/life_engine.cpp
@@ -1,3 +1,32 @@
 #include <life_engine.h>
 
 
+template<typename ElementType>
+LifeEngineBase<ElementType>::LifeEngineBase(std::size_t field_width, std::size_t field_height, dist_funct f) :
+    width_(field_width), height_(field_height)
+{
+    field_.reserve(field_height * field_width);
+
+    for (size_t i = 0; i < field_width * field_height; ++i)
+    {
+        size_t x = i / field_width;
+        size_t y = i - x * field_width;
+        field_.push_back(f(x, y, *this));
+    }
+}
+
+
+template<typename ElementType>
+void LifeEngineBase<ElementType>::step(long step_size)
+{
+    const bool forward = step_size >= 0;
+
+    for (long i = 0; i < abs(step_size); ++i) make_one_step(forward);
+}
+
+
+template<typename ElementType>
+void LifeEngineBase<ElementType>::make_one_step(bool forward)
+{
+
+}
